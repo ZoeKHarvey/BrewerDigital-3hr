60694786749100762ceed59bb4e8bf2c4479dc6c
Zoe <zoekharvey@gmail.com>
2020-07-26 20:58:19 -0500

    Updates

---
+++ precache-manifest.4090900757d343709688ea7a9f6a1277.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ba8045cbef10c3b0b3242ad8743ab1f",
+    "url": "/BrewerDigital-3hr/index.html"
+  },
+  {
+    "revision": "f383bbd43ee928478931",
+    "url": "/BrewerDigital-3hr/static/css/main.c1dc7dba.chunk.css"
+  },
+  {
+    "revision": "537cfc290991d51ec920",
+    "url": "/BrewerDigital-3hr/static/js/2.32752d59.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/BrewerDigital-3hr/static/js/2.32752d59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f383bbd43ee928478931",
+    "url": "/BrewerDigital-3hr/static/js/main.4e2de703.chunk.js"
+  },
+  {
+    "revision": "a82b52c5e88d5ae5c9df",
+    "url": "/BrewerDigital-3hr/static/js/runtime-main.283e8e49.js"
+  },
+  {
+    "revision": "a00e319db265d0d0152c3013cf1f41fb",
+    "url": "/BrewerDigital-3hr/static/media/logo copy.a00e319d.png"
+  }
+]);
